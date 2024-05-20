a86f2802360dd11c71888d0a7af36ec91eb8f4a4
Max Martinsen <max.martinsen@hotmail.com>
2024-05-20 15:10:25 +0200

    add

---
+++ src/components/Routes/AppRoutes.jsx
@@ -0,0 +1,15 @@
+// import { Routes, Route } from 'react-router-dom';
+// import Home from '../Home/Home';
+// import Layout from '../Layout/Layout';
+
+// function AppRoutes() {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Layout />}>
+//         <Route index element={<Home />} />
+//       </Route>
+//     </Routes>
+//   );
+// }
+
+// export default AppRoutes;
